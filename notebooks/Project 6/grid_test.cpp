7f404b50d1b870fe3ccbc40807464e876e5ffa73
Kapral67 <kapralmaxwell@gmail.com>
2020-11-28 23:07:25 -0800

    Update grid_test.cpp

--- notebooks/Project 6/grid_test.cpp
+++ notebooks/Project 6/grid_test.cpp
@@ -2,14 +2,22 @@
 #include <iostream>
 using namespace std;
 
-void draw(Grid &grid)
-{
-  grid.set(0,0,'+');
-  grid.set(59,23,'*');
+Grid::Grid(){
+    for(int col = 0; col < COLS; col++){
+    	for(int row = 0; row < ROWS; row++){
+            m_grid[col][row] = ' ';
+        }
+    }
 }
 
-int main()
-{
+Grid::set
+
+void draw(Grid &grid){
+    grid.set(0,0,'+');
+    grid.set(59,23,'*');
+}
+
+int main(){
   cerr << "Redirect the standard output to a file and make sure the output is\n"
        << "24 lines x 60 cols with a '+' in upper left & a '*' in lower right\n";
   Grid g;
