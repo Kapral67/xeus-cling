11d8b692750a046da46ad45f38341148e888893e
Kapral67 <kapralmaxwell@gmail.com>
2020-11-29 18:18:51 -0800

    Update square.cpp

--- notebooks/Project 6/square.cpp
+++ notebooks/Project 6/square.cpp
@@ -7,4 +7,22 @@
 #include <iostream>
 #include "square.h"
 
+Square::Square(int x, int y, int size){
+    m_x = x;
+    m_y = y;
+    m_size = size;
+}
 
+Square::void draw(Grid &grid){
+    for(int startX = m_x, count = 0; startX <= (m_x+size); startX++){
+        for(int startY = m_y; startY <= (m_y+size); startY++){
+            if(count == 0 || count == (m_y+size)){
+                grid.set(startX, startY, '*');
+            }else{
+                grid.set(startX, m_y, '*');
+                grid.set(startX, m_y+size, '*');
+            }
+        }
+        count++;
+    }
+}
