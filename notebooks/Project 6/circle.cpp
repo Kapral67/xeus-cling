693942c3ee6751c6064ef7fad291d25f465305ed
Kapral67 <kapralmaxwell@gmail.com>
2020-11-30 02:54:21 -0800

    circle

--- notebooks/Project 6/circle.cpp
+++ notebooks/Project 6/circle.cpp
@@ -3,3 +3,25 @@
 // CSCI 211
 // mkapral
 // circle.cpp
+
+#include <iostream?
+#include "circle.h"
+#include "shape.h"
+
+Circle::Circle(int x, int y) : Shape(x,y){
+    m_x = x;
+    m_y = y;
+}
+
+void Circle::draw(Grid &grid){
+    //set top and bottom pairs respectively
+    grid.set((m_x+1), m_y, 'o'); //left
+    grid.set((m_x+2), m_y, 'o'); //right
+    grid.set((m_x+1), (m_y+3), 'o'); //left
+    grid.set((m_x+2), (m_y+3), 'o'); //right
+    // set left and right pairs respectively (left to right indiv.)
+    grid.set(m_x, (m_y+1), 'o');
+    grid.set(m_x, (m_y+2), 'o');
+    grid.set((m_x+3), (m_y+1), 'o');
+    grid.set((m_x+3), (m_y+2), 'o');
+}
--- notebooks/Project 6/circle.h
+++ notebooks/Project 6/circle.h
@@ -3,3 +3,13 @@
 // Project 6
 // mkapral
 // circle.h
+
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+Class Circle : public Shape{
+    public:
+        Circle(int x, int y);
+        void draw(Grid &grid);
+};
+#endif
