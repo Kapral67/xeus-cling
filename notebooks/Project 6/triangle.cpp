25a91030d5e5266b09814ea7d6c5b4c2c5f3e74f
Kapral67 <kapralmaxwell@gmail.com>
2020-11-30 01:34:18 -0800

    Update triangle.cpp
    convert to grid.set()

--- notebooks/Project 6/triangle.cpp
+++ notebooks/Project 6/triangle.cpp
@@ -15,14 +15,9 @@ Triangle::Triangle(int x, int y) : Shape(x,y){
 }
 
 void Triangle::draw(Grid &grid){
-    Point top(m_x, m_y, '+');
-    top.draw(grid);
-    Point midL((m_x+1), (m_y-1), '+'), midR((m_x+1), (m_y+1), '+');
-    midL.draw(grid);
-    midR.draw(grid);
-    Point bottom[5];
-    for(int pt = 0; pt < 5; pt++){
-        bottom[pt]((m_x+2), ((m_y-2)+pt), '+');
-        bottom[pt].draw(grid);
-    } 
+    grid.set(m_x, m_y, '+');
+    grid.set((m_x+1), (m_y-1), '+');
+    grid.set((m_x+1), (m_y+1), '+');
+    for(int pt = 0; pt < 5; pt++)
+        grid.set((m_x+2), ((m_y-2)+pt), '+');
 }
