1a21491a6915467e70edf78ae5bf2ea9807f3bf9
Kapral67 <kapralmaxwell@gmail.com>
2020-11-30 00:52:47 -0800

    triangle w/ Point

--- notebooks/Project 6/triangle.cpp
+++ notebooks/Project 6/triangle.cpp
@@ -3,3 +3,26 @@
 // Project 6
 // mkapral
 // triangle.cpp
+
+#include <iostream>
+#include "triangle.h"
+#include "shape.h"
+#include "point.h"
+
+Triangle::Triangle(int x, int y) : Shape(x,y){
+    m_x = x;
+    m_y = y;
+}
+
+void Triangle::draw(Grid &grid){
+    Point top(m_x, m_y);
+    top.draw(grid);
+    Point midL((m_x+1), (m_y-1)), midR((m_x+1), (m_y+1));
+    midL.draw(grid);
+    midR.draw(grid);
+    Point bottom[5];
+    for(int pt = 0; pt < 5; pt++){
+        bottom[pt]((m_x+2), ((m_y-2)+pt));
+        bottom[pt].draw(grid);
+    } 
+}
--- notebooks/Project 6/triangle.h
+++ notebooks/Project 6/triangle.h
@@ -3,3 +3,15 @@
 // Project 6
 // mkapral
 // triangle.h
+
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include "shape.h"
+
+class Triangle: public Shape{
+    public:
+        Triangle(int x, int y);
+        void draw(Grid &grid);
+};
+#endif
